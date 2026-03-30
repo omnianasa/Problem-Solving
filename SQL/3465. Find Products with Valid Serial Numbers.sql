9f9d31ed96326f837daa2e0610d663e20462652f
omnia197 <omnia18ayman@gmail.com>
2026-03-30 19:00:53 +0200

    3465. Find Products with Valid Serial Numbers SQL

---
+++ SQL/3465. Find Products with Valid Serial Numbers.sql
@@ -0,0 +1,9 @@
+-- Write your MYSQL query statement below
+SELECT *
+FROM products
+WHERE REGEXP_LIKE(
+        description,
+        '(^| )SN[0-9]{4}-[0-9]{4}( |$)',
+        'c'
+    )
+ORDER BY (product_id) ASC;
