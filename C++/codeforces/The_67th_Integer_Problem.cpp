975f7b38a3d1fb34e56b103597cb04471ef2aeb1
omnia197 <omnia18ayman@gmail.com>
2026-04-13 17:48:32 +0200

    edit the error (wrong test 2)

---
+++ C++/codeforces/The_67th_Integer_Problem.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// int main()
+// {
+//     int n;
+//     int num;
+//     cin >> n;
+//     while (n--)
+//     {
+//         cin >> num;
+//         if (num >= 67)
+//         {
+//             cout << 67 << endl;
+//         }
+//         else if (num < 67 && num >= 0)
+//         {
+//             cout << num + 1 << endl;
+//         }
+//         else if (num >= -67 && num < 0)
+//         {
+//             cout << num - 1 << endl;
+//         }
+//         else if (num < -67)
+//         {
+//             cout << -67 << endl;
+//         }
+//     }
+// }
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        cout << x << endl;
+    }
+}
