9dee420d6a82bb9ec2564f5cd2a9b0194735f5da
omnia197 <omnia18ayman@gmail.com>
2026-05-09 21:57:59 +0300

    first flet library try

---
+++ Python/flet/calculator.py
@@ -0,0 +1,84 @@
+from dataclasses import field
+
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Calculator"
+    result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
+
+    @ft.control
+    class CalcButton(ft.Button):
+        expand: int = field(default_factory=lambda: 1)
+
+    @ft.control
+    class DigitButton(CalcButton):
+        bgcolor: ft.Colors = ft.Colors.WHITE_24
+        color: ft.Colors = ft.Colors.WHITE
+
+    @ft.control
+    class ActionButton(CalcButton):
+        bgcolor: ft.Colors = ft.Colors.ORANGE
+        color: ft.Colors = ft.Colors.WHITE
+
+    @ft.control
+    class ExtraActionButton(CalcButton):
+        bgcolor: ft.Colors = ft.Colors.BLUE_GREY_100
+        color: ft.Colors = ft.Colors.BLACK
+
+    page.add(
+        ft.Container(
+            width=350,
+            bgcolor=ft.Colors.BLACK,
+            border_radius=ft.BorderRadius.all(20),
+            padding=20,
+            content=ft.Column(
+                controls=[
+                    ft.Row(controls=[result], alignment=ft.MainAxisAlignment.END),
+                    ft.Row(
+                        controls=[
+                            ExtraActionButton(content="AC"),
+                            ExtraActionButton(content="+/-"),
+                            ExtraActionButton(content="%"),
+                            ActionButton(content="/"),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            DigitButton(content="7"),
+                            DigitButton(content="8"),
+                            DigitButton(content="9"),
+                            ActionButton(content="*"),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            DigitButton(content="4"),
+                            DigitButton(content="5"),
+                            DigitButton(content="6"),
+                            ActionButton(content="-"),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            DigitButton(content="1"),
+                            DigitButton(content="2"),
+                            DigitButton(content="3"),
+                            ActionButton(content="+"),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            DigitButton(content="0", expand=2),
+                            DigitButton(content="."),
+                            ActionButton(content="="),
+                        ],
+                    ),
+                ]
+            ),
+        )
+    )
+
+
+if __name__ == "__main__":
+    ft.run(main)
